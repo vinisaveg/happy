6074e47825c6a2d1c03830f501a1c918253aac04
Saveg <savegdesigner@gmail.com>
2020-10-15 20:42:32 -0300

    init mobile, dia 04 OK! :rocket:

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const { Navigator, Screen } = createStackNavigator();
+
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanagesDetails from "./pages/OrphanageDetails";
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanagesDetails} />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
